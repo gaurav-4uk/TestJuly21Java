7af3f4afe739ce9c5ad1052bc9aea7cba0b2c9d3
gaurav-4uk <gaurav4uk@gmail.com>
2021-09-01 18:15:35 +0100

    commit of B

--- src/p1/Branch_A/BranchATest1.java
+++ src/p1/Branch_A/BranchATest1.java
@@ -4,6 +4,8 @@ public class BranchATest1 {
 	public static void main(String[] args) {
 		System.out.println("This is Branch A Test 1");
 		System.out.println("Line 6");
-		System.out.println("Line 7");
+		System.out.println("Line 7 changed the file");
+		System.out.println("Line 8");
+		System.out.println("Line 9");
 	}
 }
